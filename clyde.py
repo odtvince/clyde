08f382c58af29767c41b433ca0a8a81418f034a7
vince <vince@ns3000714.ip-37-59-46.eu>
2015-05-05 17:25:11 +0200

    First commit

---
+++ clyde.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from flask.ext import restful
+
+app = Flask(__name__)
+api = restful.Api(app)
+
+class Pong(restful.Resource):
+    def post(self):
+       json = request.get_json()
+       json['data'][0]['status'] = 'received_by_taxi'
+       return json
+
+api.add_resource(Pong, '/hail/')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
